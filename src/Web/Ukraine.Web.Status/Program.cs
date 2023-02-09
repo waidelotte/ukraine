2484f4fb84ee3f9f457e8e42c31b5151fd677156
Vladyslav Prokhorchuk <waidelotte.advitam@gmail.com>
2023-02-09 21:14:16 +0200

    missing web status styles

--- src/Web/Ukraine.Web.Status/Program.cs
+++ src/Web/Ukraine.Web.Status/Program.cs
@@ -4,29 +4,42 @@ using Ukraine.Web.Status.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 var statusOptions = builder.Configuration.Get<StatusOptions>();
-if (statusOptions == null) throw CoreException.Exception("Unable to initialize section: root");
-if (string.IsNullOrEmpty(statusOptions.ResourcesPath)) throw CoreException.NullOrEmpty(nameof(statusOptions.ResourcesPath));
-if (string.IsNullOrEmpty(statusOptions.UiPath)) throw CoreException.NullOrEmpty(nameof(statusOptions.UiPath));
+if (statusOptions == null)
+{
+	throw CoreException.Exception("Unable to initialize section: root");
+}
+
+if (string.IsNullOrEmpty(statusOptions.ResourcesPath))
+{
+	throw CoreException.NullOrEmpty(nameof(statusOptions.ResourcesPath));
+}
+
+if (string.IsNullOrEmpty(statusOptions.UiPath))
+{
+	throw CoreException.NullOrEmpty(nameof(statusOptions.UiPath));
+}
 
 var seqOptions = builder.Configuration.GetSection(SeqOptions.SectionName).Get<SeqOptions>();
-if (seqOptions == null) throw CoreException.Exception($"Unable to initialize section: {SeqOptions.SectionName}");
+if (seqOptions == null)
+{
+	throw CoreException.Exception($"Unable to initialize section: {SeqOptions.SectionName}");
+}
 
 // builder.Host.UseUkraineSerilog(statusOptions.ServiceName!, builder.Configuration.GetSection("ApplicationLogging"));
-
 builder.Services
-    .AddHealthChecksUI(settings =>
-    {
-        settings.SetEvaluationTimeInSeconds(statusOptions.EvaluationTimeInSeconds);
-        settings.SetApiMaxActiveRequests(statusOptions.MaxActiveRequests);
-        settings.MaximumHistoryEntriesPerEndpoint(statusOptions.MaximumHistoryEntriesPerEndpoint);
-    })
-    .AddInMemoryStorage();
+	.AddHealthChecksUI(settings =>
+	{
+		settings.SetEvaluationTimeInSeconds(statusOptions.EvaluationTimeInSeconds);
+		settings.SetApiMaxActiveRequests(statusOptions.MaxActiveRequests);
+		settings.MaximumHistoryEntriesPerEndpoint(statusOptions.MaximumHistoryEntriesPerEndpoint);
+	})
+	.AddInMemoryStorage();
 
 var app = builder.Build();
 
 app.UseHealthChecksUI(config =>
 {
-    config.ResourcesPath = statusOptions.ResourcesPath;
+	config.ResourcesPath = statusOptions.ResourcesPath;
 });
 
 app.MapGet("/", () => Results.LocalRedirect(statusOptions.UiPath));
@@ -34,14 +47,14 @@ app.MapHealthChecksUI(config => config.UIPath = statusOptions.UiPath);
 
 try
 {
-    app.Logger.LogInformation("Starting Web Host [{ServiceName}]", statusOptions.ServiceName);
-    app.Run();
+	app.Logger.LogInformation("Starting Web Host [{ServiceName}]", statusOptions.ServiceName);
+	app.Run();
 }
 catch (Exception ex)
 {
-    app.Logger.LogCritical(ex, "Host terminated unexpectedly [{ServiceName}]", statusOptions.ServiceName);
+	app.Logger.LogCritical(ex, "Host terminated unexpectedly [{ServiceName}]", statusOptions.ServiceName);
 }
 finally
 {
-    Serilog.Log.CloseAndFlush();
+	Serilog.Log.CloseAndFlush();
 }
