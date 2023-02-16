7b02d0494ba427936dab3bf2afe921ccecfe5b3e
Vladyslav Prokhorchuk <waidelotte.advitam@gmail.com>
2023-02-16 19:10:57 +0200

    cleanup unused state save

--- src/Services/Example/Ukraine.Services.Example.Infrastructure/UseCases/CreateAuthor/CreateAuthorHandler.cs
+++ src/Services/Example/Ukraine.Services.Example.Infrastructure/UseCases/CreateAuthor/CreateAuthorHandler.cs
@@ -1,10 +1,8 @@
-﻿using Dapr.Client;
-using MediatR;
+﻿using MediatR;
 using Ukraine.Domain.Interfaces;
 using Ukraine.Services.Example.Domain.Enums;
 using Ukraine.Services.Example.Domain.Events;
 using Ukraine.Services.Example.Domain.Models;
-using Ukraine.Services.Example.Infrastructure.State;
 
 namespace Ukraine.Services.Example.Infrastructure.UseCases.CreateAuthor;
 
@@ -12,16 +10,13 @@ public class CreateAuthorHandler : IRequestHandler<CreateAuthorRequest, Author>
 {
 	private readonly IUnitOfWork _unitOfWork;
 	private readonly IEventBus _eventBus;
-	private readonly DaprClient _daprClient;
 
 	public CreateAuthorHandler(
 		IUnitOfWork unitOfWork,
-		IEventBus eventBus,
-		DaprClient daprClient)
+		IEventBus eventBus)
 	{
 		_unitOfWork = unitOfWork;
 		_eventBus = eventBus;
-		_daprClient = daprClient;
 	}
 
 	public async Task<Author> Handle(CreateAuthorRequest request, CancellationToken cancellationToken)
@@ -39,9 +34,6 @@ public class CreateAuthorHandler : IRequestHandler<CreateAuthorRequest, Author>
 		await repository.AddAsync(author, cancellationToken);
 
 		await _unitOfWork.SaveChangesAsync(cancellationToken);
-
-		// TODO TEMP VERSION
-		await _daprClient.SaveStateAsync("ukraine-statestore", $"author-{author.Id}", new AuthorState(author.Id, author.FullName), cancellationToken: cancellationToken);
 
 		await _eventBus.PublishAsync(new AuthorCreatedEvent(author.Id), cancellationToken);
 
