5bab87f34f49564c71a240bbb6a3b8f85a16d61a
Vladyslav Prokhorchuk <waidelotte.advitam@gmail.com>
2023-02-19 20:10:13 +0200

    cleanup overhead

--- src/Services/Example/Ukraine.Services.Example.Api/Program.cs
+++ src/Services/Example/Ukraine.Services.Example.Api/Program.cs
@@ -1,12 +1,104 @@
+using FluentValidation.AspNetCore;
+using HotChocolate;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Ukraine.Infrastructure.Configuration.Extensions;
+using Ukraine.Infrastructure.Dapr.Extensions;
+using Ukraine.Infrastructure.Hosting.Extensions;
+using Ukraine.Infrastructure.Identity.Extenstion;
+using Ukraine.Infrastructure.Serilog.Extenstion;
 using Ukraine.Persistence.EfCore.Interfaces;
+using Ukraine.Presentation.GraphQl.Extenstion;
+using Ukraine.Presentation.HealthChecks.Extenstion;
+using Ukraine.Presentation.Swagger.Extenstion;
 using Ukraine.Services.Example.Api;
-using Ukraine.Services.Example.Api.Extensions;
+using Ukraine.Services.Example.Api.Graph.Mutations;
+using Ukraine.Services.Example.Api.Graph.Queries;
+using Ukraine.Services.Example.Domain.Exceptions;
+using Ukraine.Services.Example.Infrastructure.Extensions;
+using Ukraine.Services.Example.Infrastructure.Options;
+using Ukraine.Services.Example.Persistence;
+using Ukraine.Services.Example.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddExampleApi(builder.Configuration);
-builder.Host.ConfigureHostApi(builder.Configuration);
+var loggingOptions = builder.Configuration.GetRequiredSection<ExampleLoggingOptions>(ExampleLoggingOptions.SECTION_NAME);
+
+builder.Host
+	.UseUkraineSerilog(options =>
+	{
+		options.ServiceName = Constants.SERVICE_NAME;
+		options.MinimumLevel = loggingOptions.MinimumLevel;
+		options.Override(loggingOptions.Override);
+
+		options.WriteTo = writeOptions =>
+		{
+			writeOptions.WriteToSeqServerUrl = loggingOptions.WriteToSeqServerUrl;
+		};
+	})
+	.UseUkraineServicesValidationOnBuild();
+
+var connectionString = builder.Configuration.GetConnectionString("Postgres");
+
+if (string.IsNullOrEmpty(connectionString))
+	throw ExampleException.Exception("Configuration: Postgres Connection String is null or empty");
+
+builder.Services
+	.AddInfrastructure(Constants.SERVICE_NAME, builder.Configuration)
+	.AddPersistence(connectionString, builder.Configuration);
+
+var identityOptions = builder.Configuration.GetRequiredSection<ExampleIdentityOptions>(ExampleIdentityOptions.SECTION_NAME);
+var swaggerOptions = builder.Configuration.GetRequiredSection<ExampleSwaggerOptions>(ExampleSwaggerOptions.SECTION_NAME);
+
+builder.Services.AddUkraineSwagger(options =>
+{
+	options.AddDoc(Constants.SERVICE_NAME);
+
+	if (!string.IsNullOrEmpty(identityOptions.Authority))
+		options.AddOAuth2(identityOptions.Authority, swaggerOptions.AuthScopes);
+
+	options.AddAuthorizeFilter(swaggerOptions.AuthScopes);
+});
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddControllers();
+
+builder.Services
+	.AddUkraineHealthChecks()
+	.AddUkraineDaprHealthCheck()
+	.AddUkrainePostgresHealthCheck(connectionString);
+
+var graphQlOptions = builder.Configuration.GetRequiredSection<ExampleGraphQlOptions>(ExampleGraphQlOptions.SECTION_NAME);
+
+builder.Services.AddUkraineGraphQl<ExampleContext>(options =>
+	{
+		options.IncludeExceptionDetails = graphQlOptions.IncludeExceptionDetails;
+		options.AllowIntrospection = graphQlOptions.AllowIntrospection;
+		options.IncludeInstrumentation = graphQlOptions.IncludeInstrumentation;
+		options.ExecutionMaxDepth = graphQlOptions.ExecutionMaxDepth;
+	})
+	.AddType<AuthorQueryTypeExtension>()
+	.AddType<AuthorMutationTypeExtension>()
+	.AddType<BookMutationTypeExtension>()
+	.RegisterService<IMediator>(ServiceKind.Synchronized);
+
+builder.Services
+	.AddUkraineAuthorization(options =>
+	{
+		foreach (var policy in identityOptions.Policies)
+		{
+			if (string.IsNullOrEmpty(policy.Name))
+				throw ExampleException.Exception("Policy Name is empty");
+
+			options.AddScopePolicy(policy.Name, policy.Scopes);
+		}
+	})
+	.AddUkraineJwtAuthentication(options =>
+	{
+		options.Audience = identityOptions.Audience;
+		options.Authority = identityOptions.Authority;
+		options.RequireHttps = identityOptions.RequireHttps;
+	});
 
 var app = builder.Build();
 
@@ -16,7 +108,32 @@ using (var scope = app.Services.CreateScope())
 	await context.Database.MigrateAsync();
 }
 
-app.UseExampleApi();
+if (app.Environment.IsDevelopment())
+	app.UseDeveloperExceptionPage();
+
+app
+	.UseUkraineSwagger(options =>
+	{
+		options.AddEndpoint();
+
+		if (!string.IsNullOrEmpty(swaggerOptions.OAuthClientId))
+			options.AddOAuthClientId(swaggerOptions.OAuthClientId);
+
+		options.AddOAuthAppName(Constants.SERVICE_NAME);
+	})
+	.UseUkraineAuthentication()
+	.UseUkraineAuthorization()
+	.UseCloudEvents();
+
+app.MapSubscribeHandler();
+app.UseUkraineGraphQl(options =>
+{
+	options.EnableBananaCakePop = graphQlOptions.EnableBananaCakePop;
+});
+
+app.MapControllers();
+app.UseUkraineHealthChecks();
+app.UseUkraineDatabaseHealthChecks();
 
 try
 {
