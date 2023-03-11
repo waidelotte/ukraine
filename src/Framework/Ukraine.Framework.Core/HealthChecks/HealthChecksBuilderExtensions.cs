c7b501203de68a4608d9a642b2c068ac0f947daf
Vladyslav Prokhorchuk <waidelotte.advitam@gmail.com>
2023-03-11 16:44:41 +0200

    extend framework health check ext

---
+++ src/Framework/Ukraine.Framework.Core/HealthChecks/HealthChecksBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Ukraine.Framework.Core.HealthChecks;
+
+public static class HealthChecksBuilderExtensions
+{
+	public static IHealthChecksBuilder AddDefaultCheck(
+		this IHealthChecksBuilder builder,
+		string name,
+		IEnumerable<string> tags)
+	{
+		return builder.AddCheck(name, () => HealthCheckResult.Healthy(), tags);
+	}
+}
