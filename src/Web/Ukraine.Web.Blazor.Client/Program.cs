d99cf842789cd609705316a6bc39902cb268cafa
waidelotte <waidelotte.advitam@gmail.com>
2023-03-21 00:38:38 +0200

    only blazor client with minimal auth

--- src/Web/Ukraine.Web.Blazor.Client/Program.cs
+++ src/Web/Ukraine.Web.Blazor.Client/Program.cs
@@ -8,6 +8,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddOidcAuthentication(options =>
+{
+	options.ProviderOptions.ClientId = "web-blazor";
+	options.ProviderOptions.Authority = "http://localhost:7000";
+	options.ProviderOptions.ResponseType = "code";
+
+	options.ProviderOptions.DefaultScopes.Add("service-example-graphql-scope");
+	options.ProviderOptions.DefaultScopes.Add("service-example-rest-scope");
+});
+
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
