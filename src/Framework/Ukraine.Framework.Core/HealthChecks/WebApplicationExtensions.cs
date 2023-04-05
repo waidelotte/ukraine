20742ce8a4b878a151567d22d18e1135553b34ed
waidelotte <waidelotte.advitam@gmail.com>
2023-04-05 22:10:49 +0300

    apply missing code-style

--- src/Framework/Ukraine.Framework.Core/HealthChecks/WebApplicationExtensions.cs
+++ src/Framework/Ukraine.Framework.Core/HealthChecks/WebApplicationExtensions.cs
@@ -2,16 +2,15 @@ using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
-namespace Ukraine.Framework.Core.HealthChecks
+namespace Ukraine.Framework.Core.HealthChecks;
+
+public static class WebApplicationExtensions
 {
-	public static class WebApplicationExtensions
+	public static void MapDefaultHealthChecks(this WebApplication webApplication)
 	{
-		public static void MapDefaultHealthChecks(this WebApplication webApplication)
+		webApplication.MapHealthChecks("/health", new HealthCheckOptions
 		{
-			webApplication.MapHealthChecks("/health", new HealthCheckOptions
-			{
-				ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-			});
-		}
+			ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+		});
 	}
 }
